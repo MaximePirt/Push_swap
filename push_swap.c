4c0c85f4e3773ab561438ad370d2b08f28e87a99
Maxime Pierrot <pierrot.maxime.pro@gmail.com>
2024-03-03 23:50:07 +0100

    Bases

---
+++ push_swap.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpierrot <mpierrot@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/02 17:05:35 by mpierrot          #+#    #+#             */
+/*   Updated: 2024/03/02 17:33:04 by mpierrot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_parsing (char *arg)
+{
+	int	i;
+
+	i = 0;
+    if (!arg)
+	{
+		printf("TOOBAD\n");
+        return (1);
+	}
+	while (arg[i])
+	{
+		if (arg[i] < 48 || arg[i] > 57 && arg[i] != 32)
+		{
+			printf ("TOOBAD\n");
+			return(1);
+		}
+		i++;
+	}
+	printf("ok\n");
+    return (0);
+}
+
+int	main (int argc, char **argv)
+{
+
+	int	i;
+
+	i = 1;
+	if (!argv[i])
+	printf ("No entry, tape something after the ./a.out please");
+	while (argv[i])
+	{
+    	ft_parsing (argv[i]);
+		i++;
+	}
+    return (0);
+}
